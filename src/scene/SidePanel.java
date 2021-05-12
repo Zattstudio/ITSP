6878c62e1b5469b0bae92f39b3b9904fda7c3342
Luis <Luis@DESKTOP-8A1A5SN>
2021-05-12 14:56:57 +0200

    add tree image to ui

--- src/scene/SidePanel.java
+++ src/scene/SidePanel.java
@@ -5,20 +5,38 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 import main.GameObject;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import utility.Vector2;
 
 public class SidePanel  extends GameObject{
 	
-	
-	public Button restartBtn = new Button("assets/gfx/menu/restart_button.png", new Vector2(1700, 100), 64, 64);
-	@Override
-	public void draw(Graphics gfx) {
-		gfx.fillRect(1300, 0, 620, 1080);
-		restartBtn.draw(gfx);
-		
-	}
+	Image treeImg;
+	public Button restartBtn = new Button("assets/gfx/menu/restart_button.png", new Vector2(1700, 500), 64, 64);
+
 	
 	public void panelUpdate(GameContainer gc, StateBasedGame sbg) {
+		
+	}
+        
+        public SidePanel(){
+            try {
+                    // load image 
+                    treeImg = new Image("assets/gfx/scene/ui_tree.png");
+                    treeImg.setFilter(Image.FILTER_NEAREST);
+
+            } catch (SlickException e) {
+                    // Simple catch block
+                    e.printStackTrace();
+            }
+        }
+        @Override
+        public void draw(Graphics gfx) {
+		//gfx.fillRect(1300, 0, 620, 1080);
+                
+                treeImg.draw(1920-treeImg.getWidth()*5, 0, 5);
+                
+		restartBtn.draw(gfx);
 		
 	}
 
